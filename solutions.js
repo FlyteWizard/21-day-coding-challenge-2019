47c4ed997b1bad2f52bdf2db55bbcabeec2ebab0
Dominique Charlebois <domo.charlebois@gmail.com>
2019-05-16 08:38:34 -0700

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -142,3 +142,14 @@ function calibrate() {
   calibrateY();
   calibrateZ();
 }
+
+// setSpeed
+function setSpeed(speed) {
+  let currentSpeed = parseInt(speed);
+  
+  if (currentSpeed > -1) {
+    navigation.speed = parseInt(speed); 
+  } else {
+    return "Number must be positive";
+  }
+}
