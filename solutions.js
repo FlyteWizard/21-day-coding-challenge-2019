08cb6ab2ac046e80e46b79c7482aef0d2060a79d
Dominique Charlebois <domo.charlebois@gmail.com>
2019-05-20 21:37:16 -0700

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -181,3 +181,17 @@ function configureBroadcast() {
 
 // Call configureBroadcast
 configureBroadcast();
+
+// decodeMessage()
+function decodeMessage(message) {
+  let currentMessage = message
+  .replace(/1/g, 'i')
+  .replace(/3/g, 'e')
+  .replace(/0/g, 'o')
+  .replace(/5/g, 'y')
+  .replace(/4/g, 'a')
+  .replace(/2/g, 'u')
+  .replace(/id/g, 'ld');
+
+  return currentMessage;
+}
